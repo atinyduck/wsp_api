3d5023466bbd3e0f422880464b4c23b7ef535b8a
atinyduck <jrhysmorgan@outlook.com>
2026-05-10 13:35:41 +0100

    Changed example database entries to fit the NYPD theme

--- database/init.sql
+++ database/init.sql
@@ -64,34 +64,32 @@ CREATE TABLE Notice_Violation (
 
 -- Fill tables with eaxmple data
 
-INSERT INTO Violation (Violation_Code, Violation_Description) VALUES
-('SPEED0110', 'Speeding 1-10 mph over limit'),
-('SPEED1120', 'Speeding 11-20 mph over limit'),
-('SPEED2100', 'Speeding 21+ mph over limit'),
-('DUI', 'Driving Under Influence'),
-('REDLT', 'Red Light Violation'),
-('PARK', 'Illegal Parking'),
-('SEATB', 'Seat Belt Violation');
-
 INSERT INTO Officer (Badge_Number, Secret_Hash, First_Name, Last_Name) VALUES
-('12345', '$2b$12$NDX7j1uCyk1haIi4qI3SpOW/7QjPOBPn5aDx.QfXiza74rD9.DB7.', 'John', 'Doe'), -- Password is "johndoe"
-('67890', '$2b$12$Upn5QUGbIlQpWDyN692QX.qirPZJ5AHVwBWlqevIx4czmfibYhKfe', 'Jane', 'Smith'); -- Password is "janesmith"
+('B99001', '$2b$12$NDX7j1uCyk1haIi4qI3SpOW/7QjPOBPn5aDx.QfXiza74rD9.DB7.', 'Jake', 'Peralta'), -- Password is "johndoe"
+('B99002', '$2b$12$Upn5QUGbIlQpWDyN692QX.qirPZJ5AHVwBWlqevIx4czmfibYhKfe', 'Amy', 'Santiago'); -- Password is "janesmith"
 
 INSERT INTO Driver (First_Name, Last_Name, Address, Birth_Date, License_Number, License_State) VALUES
-('Alice', 'Johnson', '123 Main St, Anytown, USA', '1985-06-15', 'D1234567', 'CA'),
-('Bob', 'Williams', '456 Oak St, Othertown, USA', '1990-09-22', 'W7654321', 'TX');
+('Raymond', 'Holt', '1234 Precinct Way, Brooklyn, NY', '1955-03-21', 'NY1234567', 'NY'),
+('Michael', 'Hitchcock', '5678 Duty Lane, Brooklyn, NY', '1972-08-14', 'NY7654321', 'NY'),
+('Norm', 'Scully', '9101 Donut St, Brooklyn, NY', '1970-11-08', 'NY1111111', 'NY'),
+('Charles', 'Boyle', '1121 Food Ave, Brooklyn, NY', '1984-07-10', 'NY2222222', 'NY');
 
 INSERT INTO Vehicle (VIN, Make, Model, Color, License_Plate, License_State) VALUES
-('1HGBH41JXMN109186', 'Honda', 'Civic', 'Blue', 'ABC1234', 'CA'),
-('2FTRX18W1XCA12345', 'Ford', 'F-150', 'Red', 'XYZ5678', 'TX');
+('2G1FB1E39D1234567', 'Chevrolet', 'Impala', 'Black', 'NYPD001', 'NY'),
+('3G5DA03E32S547894', 'Cadillac', 'DeVille', 'Beige', 'WHEEL', 'NY'),
+('1HGBH41JXMN109186', 'Honda', 'Civic', 'Blue', 'B99-001', 'NY'),
+('2FTRX18W1XCA12345', 'Ford', 'Taurus', 'Gray', 'B99-002', 'NY');
 
 INSERT INTO Correction_Notice (Violation_Date, Violation_Time, Location, Driver_ID, Officer_ID, VIN) VALUES
-('2024-01-15', '14:30:00', '5th Ave & Main St', 1, 1, '1HGBH41JXMN109186'),
-('2024-02-20', '09:15:00', 'Oak St & 2nd St', 2, 2, '2FTRX18W1XCA12345');
+('2026-01-15', '14:30:00', '5th Ave & Main St, Brooklyn', 1, 1, '2G1FB1E39D1234567'),
+('2026-02-20', '09:15:00', 'Flatbush Ave & Prospect Park, Brooklyn', 2, 2, '3G5DA03E32S547894'),
+('2026-03-10', '16:45:00', 'Atlantic Ave & Classon Ave, Brooklyn', 3, 1, '1HGBH41JXMN109186'),
+('2026-04-05', '11:20:00', 'Nostrand Ave & Myrtle Ave, Brooklyn', 4, 2, '2FTRX18W1XCA12345');
 
 INSERT INTO Notice_Violation (Notice_ID, Violation_Code) VALUES
 (1, 'SPEED0110'),
-(2, 'SEATB'),
-(2, 'DUI');
+(2, 'REDLT'),
+(3, 'PARK'),
+(4, 'SEATB');
 
 -- end of init.sql
